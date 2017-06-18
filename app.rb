6de4a6014b2bd44396dd51ed44985b755a8bd306
ebaty <ebamasa1220@gmail.com>
2017-06-18 23:13:53 +0900

    fix path setting

--- app.rb
+++ app.rb
@@ -2,11 +2,24 @@ require 'sinatra/base'
 
 module ChatDemo
   class App < Sinatra::Base
+    # to get form
     get "/" do
+      erb :"new.html"
+    end
+
+    # To post ghost 
+    post "/" do
+      # TODO: create scheme on redis
+      # TODO: and save object to redis
+      erb :"new.html"
+    end
+
+    # To select the ghosts
+    get "/index" do
       erb :"index.html"
     end
 
-    get "/assets/js/application.js" do
+    get "/assets/js/ws.js" do
       content_type :js
       @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
       erb :"application.js"
