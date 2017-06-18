bd4c8896e3ed4a400da8611590deb7ca73ca4919
ebaty <ebamasa1220@gmail.com>
2017-06-19 00:07:58 +0900

    complete creating new message page

--- app.rb
+++ app.rb
@@ -1,28 +1,34 @@
 require 'sinatra/base'
+require 'slim'
 
 module Whisper
   class App < Sinatra::Base
+    configure do
+      enable :logging
+    end
+
     # to get form
     get "/" do
-      erb :"new.html"
+      slim :"new.html"
     end
 
     # To post ghost 
     post "/" do
       # TODO: create scheme on redis
       # TODO: and save object to redis
-      erb :"new.html"
+      puts params
+      slim :"new.html"
     end
 
     # To select the ghosts
     get "/index" do
-      erb :"index.html"
+      slim :"index.html"
     end
 
     get "/assets/js/ws.js" do
       content_type :js
       @scheme = ENV['RACK_ENV'] == "production" ? "wss://" : "ws://"
-      erb :"application.js"
+      erb :"ws.js"
     end
   end
 end
